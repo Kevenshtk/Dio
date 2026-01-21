73acb9b3fb7eca92094d5e1962f01709ac222a42
Keven <kevendicamargo@gmail.com>
2026-01-21 20:19:27 -0300

    aula sobre next

--- formacoes/react/next-app/src/app/page.js
+++ formacoes/react/next-app/src/app/page.js
@@ -1,7 +1,20 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
     <div>
-      <h1>Home</h1>
+      {session ? (
+        <>
+          <p>Olá, {session.user?.name}</p>
+          <button onClick={() => signOut("github")}>Sair</button>
+        </>
+      ) : (
+        <button onClick={() => signIn("github")}>Entrar com Github</button>
+      )}
     </div>
   );
 }
